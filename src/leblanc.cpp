61b6f012fd574d14a9fea73abfb33b55f676704b
XadillaX <admin@xcoder.in>
2016-08-10 17:42:58 +0800

    refactor: initialize leblanc.cpp

---
+++ src/leblanc.cpp
@@ -0,0 +1,19 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  leblanc.cpp
+ *
+ *    Description:  Calculate file checksum using MD5 in node.js native (start with reading a file).
+ *
+ *        Version:  1.0
+ *        Created:  2016/08/10 17时41分33秒
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  XadillaX (zkd), i@2333.moe
+ *   Organization:  Design & Development Center, Souche Inc. CHINA
+ *
+ * =====================================================================================
+ */
+#include <openssl/md5.h>
+#include <nan.h>
