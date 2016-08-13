3d30c70f098ee0661d0e935b1bdc3a352cfa9cfb
XadillaX <admin@xcoder.in>
2016-08-13 17:44:21 +0800

    refactor: initialize md5.js

---
+++ md5.js
@@ -0,0 +1,9 @@
+/**
+ * XadillaX created at 2016-08-13 17:43:25 With ♥
+ *
+ * Copyright (c) 2016 Souche.com, all rights
+ * reserved.
+ */
+"use strict";
+
+const leblanc = require("./build/Release/leblanc.node");
